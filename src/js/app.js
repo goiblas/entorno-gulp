a867588db3b9e258b1a738e36609b4c2c567a863
Jesús Olazagoitia <jesus.designer@gmail.com>
2019-04-13 11:38:18 +0200

    simplification

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,2 @@
-import jQuery from "jquery/dist/jquery";
-
-import "bootstrap/js/src/toast";
-import "bootstrap/js/src/util";
-
-
-
-(($) => {
-    console.log($);
-    $('.toast').toast('show');
-
-    $('body').on('click', () => {
-        console.log('hola body')
-    });
-
-})(jQuery);
 document.addEventListener('load', () => console.log('hey!'))
 
